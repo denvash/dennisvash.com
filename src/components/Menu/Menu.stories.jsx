e59eef31e4da9929fbe9dce5f607cf00e6eff2ac
denvash <dennisvash@gmail.com>
2019-10-13 15:12:37 +0300

    [+] storybook/viewport addon

--- src/components/Menu/Menu.stories.jsx
+++ src/components/Menu/Menu.stories.jsx
@@ -1,11 +1,21 @@
 import React from 'react';
 import { action } from '@storybook/addon-actions';
-
-import { Menu } from '@components';
+import { INITIAL_VIEWPORTS } from '@storybook/addon-viewport';
+import Menu from './Menu';
 
 export default {
   title: 'Molecules|Menu',
   component: Menu,
+  parameters: {
+    viewport: {
+      viewports: INITIAL_VIEWPORTS,
+    },
+  },
 };
 
-export const WorksForSmallScreens = () => <Menu menuOpen={true} toggleMenu={action('clicked')} />;
+export const Default = () => (
+  <>
+    <h1 style={{ width: 100 }}>Change Viewport for Menu popup</h1>
+    <Menu menuOpen={true} toggleMenu={action()} />
+  </>
+);
